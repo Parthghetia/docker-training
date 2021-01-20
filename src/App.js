fc48004074b5e780f8ed260a8744fbab2400bdb9
pa6105074 <parth.ghetia@bell.ca>
2021-01-20 06:07:12 -0500

    checking the feature branch changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to see if it reloads.
+          Edit <code>src/App.js</code> and save to see if it reloads on the feature branch.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React and checkout the feature branch
         </a>
       </header>
     </div>
